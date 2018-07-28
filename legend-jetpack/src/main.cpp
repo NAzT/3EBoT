db4de3df7c7c77b9bae9ac336242e407a0f2c93e
Nat <nat@cmmc.io>
2018-07-28 15:58:50 +0700

    add NTPModule.

--- legend-jetpack/src/main.cpp
+++ legend-jetpack/src/main.cpp
@@ -3,18 +3,23 @@
 #include <CMMC_Sensor.h>
 
 #include "modules/LCDModule.h"
+#include "modules/NTPModule.h"
 #include "modules/WiFiModule.h"
 #include "modules/MqttModule.h"
 
 CMMC_Legend os;
 LCDModule *lcdModule;
+NTPModule *ntpModule;
 
 int temp;
 void setup()
 { 
   lcdModule = new LCDModule();
+  ntpModule = new NTPModule();
+
   os.addModule(lcdModule); 
   os.addModule(new WiFiModule()); 
+  os.addModule(ntpModule); 
   os.addModule(new MqttModule()); 
   os.setup();
   Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
---
+++ legend-jetpack/src/modules/NTPModule.h
@@ -0,0 +1,29 @@
+#include <CMMC_Module.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <CMMC_Interval.h>
+
+#ifndef CMMC_NTP_MODULE_H
+#define CMMC_NTP_MODULE_H 
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+class NTPModule: public CMMC_Module {
+  public:
+    void config(CMMC_System *os, AsyncWebServer* server); 
+    void setup();
+    void loop(); 
+    void isLongPressed(); 
+    void configLoop();
+  protected:
+    void configWebServer();
+    void sendNTPpacket(IPAddress& address);
+
+  private:
+    unsigned int localPort = 2390;
+    IPAddress timeServerIP;
+    const char* ntpServerName = "time.nist.gov"; 
+    byte packetBuffer[ NTP_PACKET_SIZE]; 
+    WiFiUDP udp;
+    CMMC_Interval interval;
+};
+
+#endif
