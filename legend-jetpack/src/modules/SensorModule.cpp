9327c468c961248a3b0291287f623645f5105477
Nat <nat@cmmc.io>
2018-07-29 07:18:15 +0700

    add median filter for SensorModule.

--- legend-jetpack/src/modules/SensorModule.cpp
+++ legend-jetpack/src/modules/SensorModule.cpp
@@ -3,6 +3,57 @@
 
 extern LCDModule* lcdModule; 
 
+void swap(float &a, float &b)
+{
+  int t = a;
+  a = b;
+  b = t;
+} 
+
+int partition(float *arr, const int left, const int right)
+{
+  const int mid = left + (right - left) / 2;
+  const int pivot = arr[mid];
+  // move the mid point value to the front.
+  swap(arr[mid], arr[left]);
+  int i = left + 1;
+  int j = right;
+  while (i <= j) {
+    while (i <= j && arr[i] <= pivot) {
+      i++;
+    }
+
+    while (i <= j && arr[j] > pivot) {
+      j--;
+    }
+
+    if (i < j) {
+      swap(arr[i], arr[j]);
+    }
+  }
+
+  swap(arr[i - 1], arr[left]);
+  return i - 1;
+}
+
+void quickSort(float *arr, const int left, const int right)
+{
+  if (left >= right) {
+    return;
+  }
+
+  int part = partition(arr, left, right);
+
+  quickSort(arr, left, part - 1);
+  quickSort(arr, part + 1, right);
+}
+
+int median(float arr[], int maxValues)
+{
+  quickSort(arr, 0, maxValues - 1);
+  return arr[maxValues / 2];
+} 
+
 void SensorModule::configLoop() {
 }
 
@@ -32,14 +83,22 @@ void SensorModule::setup()
 void SensorModule::loop() 
 { 
   interval.every_ms(5000, [&]() { 
-    _temperature = bme->readTemperature();
-    _humidity = bme->readHumidity();
-    
-    Serial.println("read sensor...");
-    data1.field1 = bme->readTemperature();
-    data1.field2 = bme->readHumidity();
-    data1.field3 = bme->readPressure() / 100.0;
-    data1.field4 = bme->readAltitude(1013.25); 
+     int idx = counter % MAX_ARRAY;
+    temp_array[idx] = bme->readTemperature();
+    humid_array[idx] = bme->readHumidity(); 
+    if (counter < MAX_ARRAY) {
+      Serial.println("CASE-1");
+      _temperature = median(temp_array, idx+1);
+      _humidity = median(humid_array, idx+1); 
+      _pressure = median(pressure_array, idx+1);
+    }
+    else {
+      Serial.println("CASE-1");
+      _temperature = median(temp_array, MAX_ARRAY);
+      _humidity = median(humid_array, MAX_ARRAY);
+      _pressure = median(pressure_array, MAX_ARRAY);
+    }
+    counter++;
   });
 }
 
--- legend-jetpack/src/modules/SensorModule.h
+++ legend-jetpack/src/modules/SensorModule.h
@@ -9,6 +9,7 @@
 #include <CMMC_Interval.h>
 #include <Adafruit_BME280.h>
 
+const int MAX_ARRAY = 10;
 class SensorModule: public CMMC_Module {
   public:
     void config(CMMC_System *os, AsyncWebServer* server); 
@@ -25,9 +26,15 @@ class SensorModule: public CMMC_Module {
     Adafruit_BME280 *bme;
     CMMC_SENSOR_DATA_T data1; 
     CMMC_Interval interval;
+    float temp_array[MAX_ARRAY] = { 0.0 };
+    float humid_array[MAX_ARRAY] = { 0.0 };
+    float pressure_array[MAX_ARRAY] = { 0.0 };
 
     float _temperature;
     float _humidity;
+    float _pressure;
+
+    unsigned long counter = 0;
 };
 
 #endif
